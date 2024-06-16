f43660bb57be1ab5c03c4c99a860352ac623c516
Svenbytes <github@svenfrank.eu>
2024-06-16 21:20:15 +0200

    Design hinzugefügt
    - Produktkarten hinzugefügt
    - Login check komprimiert

--- store.php
+++ store.php
@@ -2,11 +2,9 @@
 session_start();
  
  
-if (isset($_SESSION['email'])) {
-   echo "Herzlich Willkommen ".$_SESSION['email'];
-} else {
+ if (!isset($_SESSION['email'])) {
     header("Location: login.php?msg=1");
-}
+ }
 ?>
 
 <!doctype html>
@@ -34,6 +32,62 @@ if (isset($_SESSION['email'])) {
         <?php include('header.php'); ?>
         </header>
         <main>
+
+        <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-body-tertiary">
+            <div class="col-md-6 p-lg-5 mx-auto my-5">
+            <h1 class="display-3 fw-bold">Designed for engineers</h1>
+            <h3 class="fw-normal text-muted mb-3">Build anything you want with Aperture</h3>
+            <div class="d-flex gap-3 justify-content-center lead fw-normal">
+                <a class="icon-link" href="#">
+                Learn more
+                <svg class="bi"><use xlink:href="#chevron-right"></use></svg>
+                </a>
+                <a class="icon-link" href="#">
+                Buy
+                <svg class="bi"><use xlink:href="#chevron-right"></use></svg>
+                </a>
+            </div>
+            </div>
+            <div class="product-device shadow-sm d-none d-md-block"></div>
+            <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+        </div>
+
+
+
+            <div class="album py-5 bg-body-tertiary">
+                <div class="container">
+
+                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                    
+            <?php for ($x = 0; $x < 6; $x++) { ?>
+                <div class="col">
+                    <div class="card shadow-sm">
+                    <img src="https://picsum.photos/800/400" class="img-fluid border rounded-3 shadow-lg mb-4" alt="Example image" width="700" height="500" loading="lazy">                        <div class="card-body">
+                        <p class="card-text">Beschreibung hier.</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                            <button type="button" class="btn btn-sm btn-outline-secondary">Ansehen</button>
+                            <button type="button" class="btn btn-sm btn-outline-secondary">Warenkorb</button>
+                            </div>
+                            <small class="text-body-secondary">Preis €€</small>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+            <?php } ?>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+
+
+
+
+
 
         </main>
         <footer>
