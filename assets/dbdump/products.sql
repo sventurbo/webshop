d88354dcf9da187ebe183a9c619894a812df210b
sven <94544175+sventurbo@users.noreply.github.com>
2024-06-18 01:12:15 +0200

    Update products.sql

--- assets/dbdump/products.sql
+++ assets/dbdump/products.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Erstellungszeit: 17. Jun 2024 um 18:24
--- Server-Version: 10.4.28-MariaDB
--- PHP-Version: 8.2.4
+-- Host: 127.0.0.1
+-- Erstellungszeit: 18. Jun 2024 um 01:10
+-- Server-Version: 10.4.32-MariaDB
+-- PHP-Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -42,8 +42,7 @@ CREATE TABLE `products` (
 INSERT INTO `products` (`id`, `title`, `description`, `price`, `img`) VALUES
 (1, 'Coca Cola', '0,33l Dose', 5.99, 'https://i5.walmartimages.com/asr/14513f7a-1fa0-4971-94f9-affcfbe4a283_1.6abbc8252177d0448fe8485a1eef5605.jpeg'),
 (2, 'Lays Chips ', 'Classic Style 226.8g', 3.99, 'https://images.heb.com/is/image/HEBGrocery/001865742'),
-(3, 'FANTA UVA LATA / GRAPE SOFT DRINK', 'CANNED 350ML', 2.99, 'https://cdn.shopify.com/s/files/1/0057/2574/9317/products/kit-5-refrigerante-fanta-uva-lata-350ml-D_NQ_NP_901380-MLB27344884945_052018-F_1024x1024@2x.jpg?v=1611597105'),
-(4, 'test produkt 4', 'test produkt 4', 19.99, '');
+(3, 'Fanta Uva Lata/ Grape soft drink', 'Dose 350ml', 2.99, 'https://cdn.shopify.com/s/files/1/0057/2574/9317/products/kit-5-refrigerante-fanta-uva-lata-350ml-D_NQ_NP_901380-MLB27344884945_052018-F_1024x1024@2x.jpg?v=1611597105');
 
 --
 -- Indizes der exportierten Tabellen
